9315c7c1d2cf169cf714760606d07fa99eddfa13
Zhonghan <nzhcom123@qq.com>
2021-06-25 13:07:30 +0800

    fair 6.25

--- train.sh
+++ train.sh
@@ -60,19 +60,19 @@
 #echo "结束训练......"
 
 #Fair ST（248）
-echo "开始训练 1......"
-python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.1 --lamda 1
-wait
-echo "开始训练 2......"
-python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.3 --lamda 1
-wait
-echo "开始训练 3......"
-python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.5 --lamda 1
-wait
-echo "开始训练 4......"
-python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.8 --lamda 1 --epoch 500 --save-freq 100
-wait
-echo "结束训练......"
+#echo "开始训练 1......"
+#python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.1 --lamda 1
+#wait
+#echo "开始训练 2......"
+#python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.3 --lamda 1
+#wait
+#echo "开始训练 3......"
+#python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.5 --lamda 1
+#wait
+#echo "开始训练 4......"
+#python train_trades_cifar10.py --fair v1a --AT-method ST --batch-size 256 --T 0.8 --lamda 1 --epoch 500 --save-freq 100
+#wait
+#echo "结束训练......"
 
 # train_trades_cifar10.py --fair v1a --AT-method TRADES --batch-size 10 --T 0.8 --lamda 1
 # Fair AT
@@ -80,6 +80,25 @@ echo "结束训练......"
 # AT
 #python train_trades_cifar10.py --AT-method TRADES --batch-size 128 --T 0.1 --lamda 1
 #wait
+
+
+# ST remove label
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 12
+8 --rmlabel 5 --percent 0.2
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 12
+8 --rmlabel 5 --percent 0.5
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 12
+8 --rmlabel 5 --percent 0.7
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 12
+8 --rmlabel 5 --percent 0.9
+wait
+echo "结束训练......"
 
 
 
