5821ecdb513e65f04e39f8a19a331cccb1bbe5d2
Zhonghan <nzhcom123@qq.com>
2021-06-25 17:28:38 +0800

    fair 6.25

--- train_trades_cifar10-rmlabel.py
+++ train_trades_cifar10-rmlabel.py
@@ -106,7 +106,7 @@ transform_test = transforms.Compose([
     # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 # remove label data (default label = 3)
-trainset = CIFAR10MY(root='../data', train=True, download=True, transform=transform_train, args=args)
+trainset = CIFAR10RM(root='../data', train=True, download=True, transform=transform_train, args=args)
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
 
 testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
