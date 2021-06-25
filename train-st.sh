f7bca54c1d8622e676e234b94340611572d9abdf
Zhonghan <nzhcom123@qq.com>
2021-06-25 13:48:02 +0800

    fair 6.25

--- train-st.sh
+++ train-st.sh
@@ -14,6 +14,57 @@ CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet -
 wait
 echo "结束训练......"
 
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 6 --percent 0.2 --gpu-id 0
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 6 --percent 0.5 --gpu-id 0
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 6 --percent 0.7 --gpu-id 0
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 6 --percent 0.9 --gpu-id 0
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 7 --percent 0.2 --gpu-id 0
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 7 --percent 0.5 --gpu-id 0
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 7 --percent 0.7 --gpu-id 0
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 7 --percent 0.9 --gpu-id 0
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 8 --percent 0.2 --gpu-id 0
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 8 --percent 0.5 --gpu-id 0
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 8 --percent 0.7 --gpu-id 0
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 8 --percent 0.9 --gpu-id 0
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 9 --percent 0.2 --gpu-id 0
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 9 --percent 0.5 --gpu-id 0
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 9 --percent 0.7 --gpu-id 0
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 9 --percent 0.9 --gpu-id 0
+wait
+echo "结束训练......"
 
 
 
--- train-st2.sh
+++ train-st2.sh
@@ -2,6 +2,45 @@
 
 # ST remove label
 echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 0 --percent 0.2 --gpu-id 1
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 0 --percent 0.5 --gpu-id 1
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 0 --percent 0.7 --gpu-id 1
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 0 --percent 0.9 --gpu-id 1
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 1 --percent 0.2 --gpu-id 1
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 1 --percent 0.5 --gpu-id 1
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 1 --percent 0.7 --gpu-id 1
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 1 --percent 0.9 --gpu-id 1
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 2 --percent 0.2 --gpu-id 1
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 2 --percent 0.5 --gpu-id 1
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 2 --percent 0.7 --gpu-id 1
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 2 --percent 0.9 --gpu-id 1
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
 CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 3 --percent 0.2 --gpu-id 1
 wait
 echo "开始训练 2......"
@@ -11,6 +50,19 @@ CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet -
 wait
 echo "开始训练 4......"
 CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 3 --percent 0.9 --gpu-id 1
+wait
+echo "结束训练......"
+
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 4 --percent 0.2 --gpu-id 1
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 4 --percent 0.5 --gpu-id 1
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 4 --percent 0.7 --gpu-id 1
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=1 python train_st_cifar10-rmlabel.py --model preactresnet --AT-method ST --batch-size 128 --rmlabel 4 --percent 0.9 --gpu-id 1
 wait
 echo "结束训练......"
 
