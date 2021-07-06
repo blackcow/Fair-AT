ece194baf45d1d3faa9df1b0a74fbef7f3d83e6d
Zhonghan <nzhcom123@qq.com>
2021-07-06 16:57:04 +0800

    v7.6

--- train-rmlabelAT.sh
+++ train-rmlabelAT.sh
@@ -1,25 +1,41 @@
 #!/usr/bin/env zsh
 
-# ST remove label
+# AT remove label(0-9)
 echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 0
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 0 --seed 2
 wait
 echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 1
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 1 --seed 2
 wait
 echo "开始训练 3......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 6
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 2 --seed 2
 wait
 echo "开始训练 4......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 7
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 3 --seed 2
 wait
 echo "开始训练 5......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 8
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 4 --seed 2
 wait
 echo "开始训练 6......"
-CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 9
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 5 --seed 2
+wait
+echo "开始训练 7......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 6 --seed 2
+wait
+echo "开始训练 8......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 7 --seed 2
+wait
+echo "开始训练 9......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 8 --seed 2
+wait
+echo "开始训练 10......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 9 --seed 2
 wait
 echo "结束训练......"
+
+
+
+
 
 
 
---
+++ train-rmlabelAT2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env zsh
+
+# AT remove label(0-9)
+echo "开始训练 1......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 0 --seed 3
+wait
+echo "开始训练 2......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 1 --seed 3
+wait
+echo "开始训练 3......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 2 --seed 3
+wait
+echo "开始训练 4......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 3 --seed 3
+wait
+echo "开始训练 5......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 4 --seed 3
+wait
+echo "开始训练 6......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 5 --seed 3
+wait
+echo "开始训练 7......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 6 --seed 3
+wait
+echo "开始训练 8......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 7 --seed 3
+wait
+echo "开始训练 9......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 8 --seed 3
+wait
+echo "开始训练 10......"
+CUDA_VISIBLE_DEVICES=2,3 python train_trades_cifar10-rmlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --rmlabel 9 --seed 3
+wait
+echo "结束训练......"
+
+
+
+
+
+
+
+
+
