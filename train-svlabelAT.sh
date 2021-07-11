8757016e742c9f113e3cc79a552621e3325b22fb
Zhonghan <nzhcom123@qq.com>
2021-07-11 12:30:01 +0800

    fair 7.11

--- train-svlabelAT.sh
+++ train-svlabelAT.sh
@@ -2,10 +2,10 @@
 
 # AT remove label(0-9)
 echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=0,1 python train_trades_cifar10-svlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --seed 1 --save-label [3,5]
+CUDA_VISIBLE_DEVICES=0,1 python train_trades_cifar10-svlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --seed 1 --save-label "35"
 wait
 echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=0,1 python ttrain_trades_cifar10-svlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --seed 1 --save-label [2,3,4,5]
+CUDA_VISIBLE_DEVICES=0,1 python ttrain_trades_cifar10-svlabel.py  --model preactresnet --AT-method TRADES --batch-size 128 --seed 1 --save-label "2345"
 wait
 echo "结束训练......"
 
