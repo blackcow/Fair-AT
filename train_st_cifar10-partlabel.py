298387b0f6c6c029377b9741576cfff24ae939ea
Zhonghan <nzhcom123@qq.com>
2021-07-20 10:53:15 +0800

    fair 7.20

---
+++ train_st_cifar10-partlabel.py
@@ -0,0 +1,235 @@
+"""
+只保存部分 training data，查看 acc 变化情况
+"""
+from __future__ import print_function
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torch.optim as optim
+from torchvision import datasets, transforms
+import time
+import logging
+
+from models.wideresnet import *
+from models.densenet import *
+from models.preactresnet import create_network
+from models.resnet import *
+
+from trades import trades_loss
+from cifar10_keeplabel import CIFAR10KP
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR TRADES Adversarial Training')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--test-batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for testing (default: 128)')
+# parser.add_argument('--epochs', type=int, default=76, metavar='N',
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train')
+parser.add_argument('--weight-decay', '--wd', default=2e-4,
+                    type=float, metavar='W')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--gpu-id', type=str, default='0', help='gpu_id')
+parser.add_argument('--epsilon', default=0.031, help='perturbation')
+parser.add_argument('--num-steps', default=10,
+                    help='perturb number of steps')
+parser.add_argument('--step-size', default=0.007,
+                    help='perturb step size')
+parser.add_argument('--beta', default=6.0,
+                    help='regularization, i.e., 1/lambda in TRADES')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--model-dir', default='./model-cifar-wideResNet/',
+                    help='directory of model for saving checkpoint')
+parser.add_argument('--save-freq', '-s', default=20, type=int, metavar='N',
+                    help='save frequency')
+parser.add_argument('--model', default='wideresnet', choices=['wideresnet', 'densenet', 'preactresnet'],
+                    help='AT model name')
+parser.add_argument('--AT-method', type=str, default='ST',
+                    help='AT method', choices=['TRADES', 'PGD', 'ST'])
+
+# model factors
+parser.add_argument('--depth', type=int, default=34, metavar='N',
+                    help='model depth (default: 34)')
+parser.add_argument('--widen_factor', type=int, default=10, metavar='N',
+                    help='model widen_factor (default: 10)')
+parser.add_argument('--droprate', type=float, default=0.0, metavar='N',
+                    help='model droprate (default: 0.0)')
+# keep label data
+parser.add_argument('--percent', default=0.1, type=float, help='Percentage of deleted data')
+
+args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+print(args)
+# settings
+
+model_dir = args.model_dir + args.model + '/' + args.AT_method + '/kplabel' + '/percent_'+str(args.percent)
+print(model_dir)
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+
+
+use_cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+device = torch.device("cuda" if use_cuda else "cpu")
+kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
+
+# setup data loader
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+trainset = CIFAR10KP(root='../data', train=True, download=True, transform=transform_train, args=args)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, **kwargs)
+testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform_test)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+
+def train(args, model, device, train_loader, optimizer, epoch, logger):
+# def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.cuda(), target.cuda()
+
+        optimizer.zero_grad()
+        # Standard Training Loss
+        _, out = model(data)
+        loss = F.cross_entropy(out, target)
+
+        loss.backward()
+        optimizer.step()
+
+        # print progress
+        if batch_idx % args.log_interval == 0:
+            logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+def eval_train(model, device, train_loader, logger):
+    model.eval()
+    train_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in train_loader:
+            data, target = data.cuda(), target.cuda()
+            _, output = model(data)
+            train_loss += F.cross_entropy(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    train_loss /= len(train_loader.dataset)
+    logger.info('Training: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        train_loss, correct, len(train_loader.dataset),
+        100. * correct / len(train_loader.dataset)))
+    training_accuracy = correct / len(train_loader.dataset)
+    return train_loss, training_accuracy
+
+
+def eval_test(model, device, test_loader, logger):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.cuda(), target.cuda()
+            _, output = model(data)
+            test_loss += F.cross_entropy(output, target, size_average=False).item()
+            pred = output.max(1, keepdim=True)[1]
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    logger.info('Test: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    test_accuracy = correct / len(test_loader.dataset)
+    return test_loss, test_accuracy
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """decrease the learning rate"""
+    lr = args.lr
+    if epoch >= 75:
+        lr = args.lr * 0.1
+    if epoch >= 90:
+        lr = args.lr * 0.01
+    if epoch >= 100:
+        lr = args.lr * 0.001
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+
+def get_logger(filename, verbosity=1, name=None):
+    level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
+    formatter = logging.Formatter("%(message)s")
+    logger = logging.getLogger(name)
+    logger.setLevel(level_dict[verbosity])
+
+    fh = logging.FileHandler(filename, "w")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+
+    return logger
+
+def main():
+    # init model, ResNet18() can be also used here for training
+    if args.model == 'wideresnet':
+        model = nn.DataParallel(
+            WideResNet(depth=args.depth, widen_factor=args.widen_factor, dropRate=args.droprate)).cuda()
+    elif args.model == 'densenet':
+        model = nn.DataParallel(DenseNet121().cuda())
+    elif args.model == 'preactresnet':  # model 小，需要降 lr
+        model = nn.DataParallel(create_network().cuda())
+        args.lr = 0.01
+        args.weight_decay = 5e-4
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    logger = get_logger(model_dir + '/train.log')
+
+    for epoch in range(1, args.epochs + 1):
+        # adjust learning rate for SGD
+        adjust_learning_rate(optimizer, epoch)
+
+        # adversarial training
+        start = time.time()
+        train(args, model, device, train_loader, optimizer, epoch, logger)
+        # train(args, model, device, train_loader, optimizer, epoch)
+        end = time.time()
+        tm = (end - start)/60
+        print('时间(分钟):' + str(tm))
+        # evaluation on natural examples
+        print('================================================================')
+        eval_train(model, device, train_loader, logger)
+        eval_test(model, device, test_loader, logger)
+        print('================================================================')
+
+        # save checkpoint
+        if epoch % args.save_freq == 0 or epoch == 76:
+            torch.save(model.state_dict(),
+                       os.path.join(model_dir, 'model-wideres-epoch{}.pt'.format(epoch)))
+            # torch.save(optimizer.state_dict(),
+            #            os.path.join(model_dir, 'opt-wideres-checkpoint_epoch{}.pt'.format(epoch)))
+
+
+if __name__ == '__main__':
+    main()
