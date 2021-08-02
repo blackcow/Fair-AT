878a5e9394c2392bd813e38568217acbf4e5ff53
Zhonghan <nzhcom123@qq.com>
2021-08-02 20:35:41 +0800

    fair 7.31

--- train-st-imagenet10.sh
+++ train-st-imagenet10.sh
@@ -2,19 +2,19 @@
 
 # ST keep percent data(CIFAR-100)
 echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.01 --model preactresnet --seed 1 --gpu-id 0
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.05 --model preactresnet --seed 1 --gpu-id 0
 wait
 echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.01 --model preactresnet --seed 2 --gpu-id 0
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.05 --model preactresnet --seed 2 --gpu-id 0
 wait
 echo "开始训练 3......"
-CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.01 --model preactresnet --seed 3 --gpu-id 0
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.05 --model preactresnet --seed 3 --gpu-id 0
 wait
 echo "开始训练 4......"
-CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.01 --model preactresnet --seed 4 --gpu-id 0
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.05 --model preactresnet --seed 4 --gpu-id 0
 wait
 echo "开始训练 5......"
-CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.01 --model preactresnet --seed 5 --gpu-id 0
+CUDA_VISIBLE_DEVICES=0 python train_st_cifar10_partlabel.py --dataset ImageNet10 --percent 0.05 --model preactresnet --seed 5 --gpu-id 0
 wait
 echo "结束训练......"
 #
