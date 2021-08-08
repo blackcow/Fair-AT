50a3c86c89e33449669c44ac5665d2c0b0f393cb
Zhonghan <nzhcom123@qq.com>
2021-08-08 15:06:15 +0800

    fair 7.31

--- train-at-cifar10-aug.sh
+++ train-at-cifar10-aug.sh
@@ -61,18 +61,18 @@ echo "结束训练......"
 # TRADES_aug_pgdattk (CIFAR10)
 # 在每个 seed 下使用不同的超参数
 echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 1 --beta 0.5 --beta_aug 6
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 1 --beta 0.1 --beta_aug 6
 wait
 echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 2 --beta 1 --beta_aug 6
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 2 --beta 0.5 --beta_aug 6
 wait
 echo "开始训练 3......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 3 --beta 1 --beta_aug 12
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 3 --beta 1 --beta_aug 6
 wait
 echo "开始训练 4......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 4 --beta 1 --beta_aug 18
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 4 --beta 1 --beta_aug 12
 wait
 echo "开始训练 5......"
-#CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 1 --seed 5 --beta 1 --beta_aug 18
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 5 --beta 1 --beta_aug 18
 wait
 echo "结束训练......"
