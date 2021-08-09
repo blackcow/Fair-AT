0bd99411575072b81971fe991ee0f1d88abcb4b7
Zhonghan <nzhcom123@qq.com>
2021-08-09 21:54:36 +0800

    fair 8.9

--- train-at-cifar10-aug.sh
+++ train-at-cifar10-aug.sh
@@ -60,23 +60,23 @@
 
 # TRADES_loss_adp (CIFAR10),Trade loss 根据 label 不同权重不一致
 # 在每个 seed 下使用不同的超参数
-echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 1 --beta 0.1 --beta_aug 6
-wait
-echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 2 --beta 0.5 --beta_aug 6
-wait
-echo "开始训练 3......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 3 --beta 1 --beta_aug 6
-wait
-echo "开始训练 4......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 4 --beta 1 --beta_aug 12
-wait
-echo "开始训练 5......"
-#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 5 --beta 6 --beta_aug 18
-#CUDA_VISIBLE_DEVICES=2 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 2 --seed 6 --beta 4 --beta_aug 12
-wait
-echo "结束训练......"
+#echo "开始训练 1......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 1 --beta 0.1 --beta_aug 6
+#wait
+#echo "开始训练 2......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 2 --beta 0.5 --beta_aug 6
+#wait
+#echo "开始训练 3......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 3 --beta 1 --beta_aug 6
+#wait
+#echo "开始训练 4......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 4 --beta 1 --beta_aug 12
+#wait
+#echo "开始训练 5......"
+##CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 0 --seed 5 --beta 6 --beta_aug 18
+##CUDA_VISIBLE_DEVICES=2 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_loss_adp  --dataset CIFAR10 --gpu-id 2 --seed 6 --beta 4 --beta_aug 12
+#wait
+#echo "结束训练......"
 
 # TRADES_aug (CIFAR10)
 # 在每个 seed 下使用不同的超参数
