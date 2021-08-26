908f91099eef229533b710379d3b0e07c6fa57eb
Zhonghan <nzhcom123@qq.com>
2021-08-26 09:47:41 +0800

    fair 8.23

--- train-at-cifar10-el2.sh
+++ train-at-cifar10-el2.sh
@@ -5,18 +5,27 @@
 # 在每个 seed 下使用不同的超参数
 
 echo "开始训练 1......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 1 --alpha 0.1
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 6 --alpha 0.1 --tmp 1
 wait
 echo "开始训练 2......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 2 --alpha 0.5
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 7 --alpha 1 --tmp 1
 wait
 echo "开始训练 3......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 3 --alpha 1
+CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 8 --alpha 5 --tmp 1
 wait
-echo "开始训练 4......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 4 --alpha 5
-wait
-echo "开始训练 5......"
-CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 5 --alpha 10
-wait
-echo "结束训练......"
+
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 1 --alpha 0.1
+#wait
+#echo "开始训练 2......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 2 --alpha 0.5
+#wait
+#echo "开始训练 3......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 3 --alpha 1
+#wait
+#echo "开始训练 4......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 4 --alpha 5
+#wait
+#echo "开始训练 5......"
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method TRADES_el_li2  --dataset CIFAR10 --gpu-id 1 --seed 5 --alpha 10
+#wait
+#echo "结束训练......"
