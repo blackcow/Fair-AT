feacc9b64005c99b2f2c93bdc7207a3d8d79af30
Zhonghan <nzhcom123@qq.com>
2021-09-03 22:12:56 +0800

    fair 9.3

--- train-at-cifar10-reweight.sh
+++ train-at-cifar10-reweight.sh
@@ -10,13 +10,13 @@ echo "开始训练 2......"
 CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 1 --seed 2 --reweight 0.05  --discrepancy 5
 wait
 #echo "开始训练 3......"
-#CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 1 --seed 3 --reweight 0.1  --discrepancy 3
+#CUDA_VISIBLE_DEVICES=0 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 0 --seed 3 --reweight 0.1  --discrepancy 3
 #wait
 #echo "开始训练 4......"
-#CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 1 --seed 4 --reweight 1  --discrepancy 3
+#CUDA_VISIBLE_DEVICES=2 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 2 --seed 4 --reweight 1  --discrepancy 3
 #wait
 #echo "开始训练 5......"
-#CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 1 --seed 5 --reweight 5  --discrepancy 3
+#CUDA_VISIBLE_DEVICES=3 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 3 --seed 5 --reweight 5  --discrepancy 3
 wait
 #echo "开始训练 6......"
 #CUDA_VISIBLE_DEVICES=1 python train_trades_cifar10.py --model preactresnet --AT-method AT_reweight  --dataset CIFAR10 --gpu-id 1 --seed 6 --reweight 1  --discrepancy 0
